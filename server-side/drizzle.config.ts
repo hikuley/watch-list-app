27b8d87950998c5ea55329eda0d9443f3607d812
hikuley <halil.ibrahim.kuley@revsolz.com>
2025-03-03 23:29:08 +0300

    Update database connection configuration to use individual credentials from environment variables

--- server-side/drizzle.config.ts
+++ server-side/drizzle.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
     schema: './src/db/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        host: process.env.DB_HOST!,
+        user: process.env.DB_USER!,
+        password: process.env.DB_PASSWORD!,
+        database: process.env.DB_NAME!,
+        port: Number(process.env.DB_PORT!),
+        ssl: false,
     },
 });
