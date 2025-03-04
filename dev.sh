4f47cb20de50c14b087e08dea29a2eef63592a51
hikuley <halil.ibrahim.kuley@revsolz.com>
2025-03-04 21:36:34 +0300

    Add dev.sh script to manage services (start/stop) for development environment

---
+++ dev.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script starts or stops all specified services (postgres, kafka, redis)
+# Usage: ./dev.sh [start|stop]
+# Example: ./dev.sh start
+
+# Check if exactly one argument is provided and if it is either 'start' or 'stop'
+if [ "$#" -ne 1 ] || [[ ! "$1" =~ ^(start|stop)$ ]]; then
+    echo "Usage: $0 [start|stop]"
+    exit 1
+fi
+
+# Store the action (start or stop) in a variable
+action=$1
+
+# Define an array of services to be managed
+services=("postgres" "kafka" "redis")
+
+# Inform the user about the action being performed
+echo "Performing $action for all services..."
+
+# Loop through each service and run the corresponding script with the action
+for service in "${services[@]}"; do
+    echo "Running $service-docker.sh $action"
+    ./$service-docker.sh $action
+done
+
+# Run docker ps to show the status of all containers after the action
+docker ps
